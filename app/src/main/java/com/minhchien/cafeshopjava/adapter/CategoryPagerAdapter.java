dbccf23f45b5c92a2bd9f9704df8e2324baf5d12
Minh Chiến <160015433+minhchien137@users.noreply.github.com>
2024-10-16 14:43:49 +0700

    15:30 16/10/2024 done Drink Adapter

--- app/src/main/java/com/minhchien/cafeshopjava/adapter/CategoryPagerAdapter.java
+++ app/src/main/java/com/minhchien/cafeshopjava/adapter/CategoryPagerAdapter.java
@@ -1,4 +1,33 @@
 package com.minhchien.cafeshopjava.adapter;
 
-public class CategoryPagerAdapter {
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.minhchien.cafeshopjava.fragment.DrinkFragment;
+import com.minhchien.cafeshopjava.model.Category;
+
+import java.util.List;
+
+public class CategoryPagerAdapter extends FragmentStateAdapter {
+
+    private final List<Category> listCategory;
+
+    public CategoryPagerAdapter(@NonNull FragmentActivity fragmentActivity, List<Category> list) {
+        super(fragmentActivity);
+        listCategory = list;
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        return DrinkFragment.newInstance(listCategory.get(position).getId());
+    }
+
+    @Override
+    public int getItemCount() {
+        if (listCategory != null) return listCategory.size();
+        return 0;
+    }
 }
